5f3d3ac6bcf16243c9c2515a5630d6bbd28b9e0a
ruthvij17 <130194046+ruthvij17@users.noreply.github.com>
2024-08-20 14:26:52 +0000

    Learning is in process

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
-const http =require("http");
+const http = require("http");
 
-const port=8081;
+const port = 8081;
+
+const todo = ["hii", "hello", "how"];
 
 http
-.createServer((req,res)=>{
-    res.writeHead(200,{"content-type": "text/html"});
-    res.write(`<h2>Hey server started running on a port ${port}:-)</h2>`);
+  .createServer((req, res) => {
+    const { method, url } = req;
+
+    if (url === "/todos") {
+      if (method === "GET") {
+        res.writeHead(200, { "content-Type": "text/html" });
+        res.write(todo.toString());
+      } else if (method === "POST") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.log(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+            //console.log(chunk);
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let newTodo=todo;
+            newTodo.push(body.item);
+            //console.log("data:",body);
+          });
+      } else {
+        res.writeHead(501);
+      }
+    } else {
+      res.writeHead(404);
+    }
     res.end();
-})
-.listen(port,()=>{
+  })
+  .listen(port, () => {
     console.log(`Node js server started running on port ${port}`);
-})
+  });
